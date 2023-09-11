04c6b24bb5774f1dfb17f19dc9ce3ac410921518
Fluk <88507245+64070014Jiraroth@users.noreply.github.com>
2023-09-12 01:15:53 +0700

    InternalServerError: 500
    Internal Server Error from GET http://localhost:8080/wizards

---
+++ lab06/controller/WizardController.java
@@ -0,0 +1,56 @@
+package com.example.lab06.controller;
+
+import com.example.lab06.pojo.Wizard;
+import com.example.lab06.repository.WizardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+public class WizardController {
+
+    @Autowired
+    private WizardService wizardService;
+    @RequestMapping(value = "/wizards", method = RequestMethod.GET)
+    public ResponseEntity<?> getWizard() {
+        List<Wizard> wizards = wizardService.retrieveWizards();
+        return ResponseEntity.ok(wizards);
+    }
+
+    @RequestMapping(value = "/addWizard", method = RequestMethod.POST)
+    public ResponseEntity<?> addWizard(
+            @RequestParam("name") String name,
+            @RequestParam("gender") String gender,
+            @RequestParam("dollars") Double dollars,
+            @RequestParam("school") String school,
+            @RequestParam("house") String house,
+            @RequestParam("position") String position){
+        Wizard n = wizardService.addWizard(new Wizard(null,gender, name, school, house, dollars, position));
+        return ResponseEntity.ok(n);
+    }
+    @RequestMapping(value = "/updateWizard", method = RequestMethod.POST)
+    public boolean updateWizard(
+            @RequestParam("oldName") String oldName,
+            @RequestParam("gender") String gender,
+            @RequestParam("name") String name,
+            @RequestParam("school") String school,
+            @RequestParam("house") String house,
+            @RequestParam("money") Double money,
+            @RequestParam("position") String position){
+        Wizard wizard = wizardService.retrieveWizardByName(oldName);
+        if (wizard != null) {
+            wizardService.updateWizard(new Wizard(wizard.get_id(), gender, name, school, house, money, position));
+            return true;
+        }
+        else { return false; }
+    }
+    @RequestMapping(value = "/deleteWizard/{name}", method = RequestMethod.POST)
+    public boolean deleteWizard(@PathVariable("name")String name) {
+        Wizard wizard = wizardService.retrieveWizardByName(name);
+        return wizardService.deleteWizard(wizard);
+    }
+
+
+}
---
+++ lab06/pojo/Wizards.java
@@ -0,0 +1,10 @@
+package com.example.lab06.pojo;
+
+import java.util.ArrayList;
+
+public class Wizards {
+    ArrayList<Wizard> model = new ArrayList<Wizard>();
+    public Wizards() {
+//        ArrayList<Wizard> model = new ArrayList<Wizard>();
+    }
+}
